9b51af7a4868aefaff892a92e98b76df0c65ea67
varseienko <varseienko@dio211.dio-soft.local>
2015-02-20 16:14:58 +0200

    Initial revision

---
+++ src/main/java/com/dio/javamentoring/warehouse/App.java
@@ -0,0 +1,13 @@
+package com.dio.javamentoring.warehouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
